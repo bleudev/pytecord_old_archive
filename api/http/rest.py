a8292ef54c6670b251587ab45f45787b7793264b
itttgg <spdd5724@gmail.com>
2022-03-17 08:04:36 +0300

    adding rest support

---
+++ api/http/rest.py
@@ -0,0 +1,17 @@
+import requests
+
+class Rest:
+    def __init__(self, token):
+        self.token = token
+
+    def get(self, goal: str, id: int):
+        if goal.casefold() == 'guild':
+            return requests.get(f'https://discord.com/api/v10/guilds/{str(id)}',
+                                headers={'Authorization': f'Bot {self.token}'}).json()
+        elif goal.casefold() == 'channel':
+            return requests.get(f'https://discord.com/api/v10/channels/{str(id)}',
+                                headers={'Authorization': f'Bot {self.token}'}).json()
+
+    def fetch(self, channel_id, message_id):
+        return requests.get(f'https://discord.com/api/v10/channels/{str(channel_id)}/messages/{str(message_id)}',
+                            headers={'Authorization': f'Bot {self.token}'}).json()
