02307b18d5ccf099ff211217f64a5ad1dfeddb4f
itttgg <spdd5724@gmail.com>
2022-03-22 12:12:34 +0300

    Adding method tojson()

--- dispy/embed.py
+++ dispy/embed.py
@@ -1,8 +1,15 @@
 class DisField:
-    def __init__(self, title: str, value: str):
-        self.title = title
+    def __init__(self, name: str, value: str, inline: bool = True):
+        self.name = name
         self.value = value
+        self.inline = inline
 
+    def tojson(self):
+        return {
+            "name": self.name,
+            "value": self.value,
+            "inline": self.inline
+        }
 
 class DisEmbed:
     def __init__(self, title: str, description: str = None, color=0xffffff, footer: str = None):
@@ -18,3 +25,17 @@ class DisEmbed:
 
     def add_field(self, field: DisField):
         self.fields.append(field)
+
+    def tojson(self):
+        fields_jsons = []
+
+        for f in self.fields:
+            fields_jsons.append(f.tojson())
+
+        return {
+            "title": self.title,
+            "description": self.description,
+            "footer": self.footer,
+            "color": self.color,
+            "fields": fields_jsons
+        }
