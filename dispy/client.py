7c3c43c54880c53ba903f5a9c7d2c6966d5ca1eb
itttgg <spdd5724@gmail.com>
2022-03-22 19:23:08 +0300

    on_ready adding

--- dispy/client.py
+++ dispy/client.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from .err import Errors as Err
 from .http.rest import Rest
 from .channel import Channel
@@ -6,19 +8,35 @@ from .channel import Channel
 class DisBot:
     def __init__(self, token: str, prefix="!"):
         self._rest = Rest(token)
+        self.isready = False
         if prefix == "" or " " in prefix:
             Err.raiseerr(Err.DisBotInitErr)
         else:
             self.prefix = prefix
 
-    def event(self, type: str):
+    async def on_ready(self):
+        return
+
+    def on_message(self, message):
+        return
+
+    def on(self, type: str):
         def wrapper(func):
             if type == "messagecreate":
                 self.on_message = func
             elif type == "ready":
                 self.on_ready = func
             else:
-                Err.raiseerr(Err.DisBotEventErr)
+                print("Error in on() - Invalid type of event (read docs)")
+
+    def run(self):
+        self.isready = True
+
+        async def loop():
+            if self.isready:
+                asyncio.run(self.on_ready())
+
+        asyncio.run(loop())
 
     def get_channel(self, id: int):
         return Channel(self._rest.get('channel', id), self._rest)
