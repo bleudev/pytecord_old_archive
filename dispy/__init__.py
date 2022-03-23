d7abb10ad32b1fd95d72b2b6b1e12d5983872c0f
itttgg <spdd5724@gmail.com>
2022-03-23 10:25:26 +0300

    Deleting types.py

--- dispy/__init__.py
+++ dispy/__init__.py
@@ -2,7 +2,6 @@ from .client import *
 from .err import *
 from .guild import *
 from .channel import *
-from .types import *
 from .embed import *
 from .color import *
 
--- dispy/types.py
+++
@@ -1,3 +0,0 @@
-class DisBotEventType:
-    on_message = "messagecreate"
-    on_ready = "ready"
