1944d3f423d5062e4452c9f9b8b01dc07df36553
itttgg <spdd5724@gmail.com>
2022-03-23 10:22:53 +0300

    Adding types, package variables and docs

--- dispy/__init__.py
+++ dispy/__init__.py
@@ -5,3 +5,24 @@ from .channel import *
 from .types import *
 from .embed import *
 from .color import *
+
+__version__ = "0.1beta"
+__github__ = "https://github.com/itttgg/dispy"
+__packagename__ = "dispy"
+
+
+# Types for simpler creating bots
+class DisBotEventType:  # Event type for DisBot
+    """
+    __descripton__
+    This class created for simplification adding events to DisBot
+
+    __variables__(It's constants)
+    @ON_MESSAGE will be called when new message was created in DisBot.guild.channels
+    @ON_READY will be called when bot becomes ready
+
+    __class__
+    This is class, not an object
+    """
+    ON_MESSAGE = "messagecreate"
+    ON_READY = "ready"
