897f37d928c91ef4b62304ed62ec800d9b85ab47
itttgg <spdd5724@gmail.com>
2022-03-24 09:39:32 +0300

    DisMessage edits

--- dispy/message.py
+++ dispy/message.py
@@ -1,3 +1,11 @@
+from .channel import DisChannel
+from .guild import DisGuild
+import dispy.http.rest
+
+
 class DisMessage:
-    def __init__(self, data):
+    def __init__(self, data, rest: dispy.http.rest.Rest):
         self.id = data["id"]
+        self._rest = rest
+        self.channel = DisChannel(self._rest.get("channel", data["channel_id"]), rest)
+        self.guild = DisGuild(self._rest.get("guild", data["guild_id"]), rest)
