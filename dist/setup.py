4d15ce924323ce66e3736e48c019a67fb25c91b3
itttgg <spdd5724@gmail.com>
2022-04-10 22:49:15 +0300

    Adding setup.py

---
+++ dist/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.1',
+      description='Package for creating Discord bots',
+      author='itttgg',
+      packages=['aiohttp', 'requests']
+     )
