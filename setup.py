f186f9979339536c793f6ddcd4d7fd54087060c6
itttgg <spdd5724@gmail.com>
2022-04-11 11:01:00 +0300

    Preparing for new pre release

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import dist.dispy
 
 setup(
-      name="Dispy",
-      version="0.1a",
-      download_url=dist.dispy.__stablever__,
+      name="dispy",
+      version="0.1d",
       author='itttgg',
+      author_email="aitiiigg1@gmail.com"
       python_requires='>=3.6.0',
-      license="MIT"
+      license="MIT",
+      classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License"
+      ],
+      package_dir={"": "dispy"},
+      packages=find_packages(where="ontology_processing")
 )
