5656c92a23ef3577db880322e5604928fdb41da2
itttgg <spdd5724@gmail.com>
2022-04-11 13:02:10 +0300

    Fixed builds #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="dispy",
+    version="0.1.0",
+    description="Dispy - package for creating bots for Discord",
+    license="MIT",
+    author="itttgg"
+)
