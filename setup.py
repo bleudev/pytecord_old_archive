96913f2ed83e5d87777529323c33201538b30346
itttgg <spdd5724@gmail.com>
2022-04-11 13:09:25 +0300

    Fixed builds #9

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="dispy",
     version="0.1.0",
     description="Dispy - package for creating bots for Discord",
     license="MIT",
-    author="itttgg"
+    author="itttgg",
+    packages=[find_packages(where="dispy", include=["https"])]
 )
