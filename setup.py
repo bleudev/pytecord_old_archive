877c8b040c78b947d0c321a16a628b619418684b
itttgg <spdd5724@gmail.com>
2022-04-12 13:05:55 +0300

    Build fixes

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as mdf:
+    long_description = mdf.read()
+
+requires = []
+
+with open("requirements.txt", "r", encoding="utf-8") as rf:
+    requires.append(str(rf.readline()))
 
 setup(
-    name="disspy",
-    version="0.1.0",
-    description="Dispy - package for creating bots for Discord",
-    license="MIT",
+    name="cf-remote",
+    version='0.1a',
     author="itttgg",
-    packages=["dispy"]
+    description="Dispy - package for creating bots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/itttgg/dispy",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=requires
 )
