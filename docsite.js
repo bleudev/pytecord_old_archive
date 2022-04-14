fc858620f8e23e72a7120549996a1ceea8c0d4be
itttgg <spdd5724@gmail.com>
2022-04-15 00:09:20 +0300

    Remove site to another branch

--- docsite.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-app.use(express.static(path.join(process.cwd(), 'docsite')));
-console.log(process.env.PORT || 5000);
-app.listen(process.env.PORT || 5000);
