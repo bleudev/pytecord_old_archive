2238afb7cc8459deca700852f4bf2157724265dd
itttgg <spdd5724@gmail.com>
2022-06-28 20:40:25 +0300

    Update examples

---
+++ examples/bot_on_reaction.py
@@ -0,0 +1,12 @@
+import disspy
+
+bot = disspy.DisBot(token="TOKEN", application_id=0, flags=disspy.DisFlags.reactions())  # Create bot
+
+
+# "On reaction add" event
+@bot.on(disspy.DisBotEventType.ON_REACTION)
+async def on_reaction(reaction: disspy.DisReaction):
+    print(reaction.user.fullname)
+
+
+bot.run()  # Running bot
