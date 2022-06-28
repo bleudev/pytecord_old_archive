d88e66a50556d07ee8bf65dd22de7ccf67deb466
itttgg <spdd5724@gmail.com>
2022-06-28 21:18:58 +0300

    Update examples №2

--- examples/bot_on_reaction.py
+++ examples/bot_on_reaction.py
@@ -9,4 +9,10 @@ async def on_reaction(reaction: disspy.DisReaction):
     print(reaction.user.fullname)
 
 
+# "On reaction remove" event
+@bot.on(disspy.DisBotEventType.ON_REACTIONR)
+async def on_reactionr(reaction: disspy.DisRemovedReaction):
+    print(reaction.emoji.unicode)
+
+
 bot.run()  # Running bot
