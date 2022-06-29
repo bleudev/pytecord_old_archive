0798973690426e1e5a330d6fe81286a5c6139623
itttgg <spdd5724@gmail.com>
2022-06-29 18:55:17 +0300

    Update examples

---
+++ examples/bot_on_typing.py
@@ -0,0 +1,19 @@
+import disspy
+
+bot = disspy.DisBot(token="TOKEN", application_id=0, flags=disspy.DisFlags.messages())  # Create bot
+
+
+# "On typing" event
+@bot.on(disspy.DisBotEventType.ON_TYPING)
+async def on_typing(user: disspy.DisUser, channel: disspy.DisChannel):
+    print(user.id)
+    await channel.send("You started typing!")
+
+
+# "On dm typing" event
+@bot.on(disspy.DisBotEventType.ON_DM_TYPING)
+async def on_dm_typing(user: disspy.DisUser, channel: disspy.DisDm):
+    print(user.id)
+    print(channel.id)
+
+bot.run()  # Running bot
