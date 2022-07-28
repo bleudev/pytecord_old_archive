78e9093a8ac19ca019d4711b4178ea6b078fa1b9
itttgg <spdd5724@gmail.com>
2022-07-28 17:32:55 +0300

    CLI developerment №2

--- disspy/__main__.py
+++ disspy/__main__.py
@@ -23,30 +23,99 @@ SOFTWARE.
 """
 
 from sys import argv
-from . import __version__
 import colorama
+from . import __version__
+
+colorama.init()
+
+__cli_version__ = "1.0"
+
+_helpcommand = f"""
+            {colorama.Fore.GREEN}Commands:
+                {colorama.Fore.YELLOW}-help, -H ->{colorama.Fore.RESET} Command with information about other commands
+                {colorama.Fore.YELLOW}-version, -V ->{colorama.Fore.RESET} Version of package
+            {colorama.Fore.GREEN}Params for commands (example, python -m disspy -help -no-color):
+                {colorama.Fore.YELLOW}-[command] ->{colorama.Fore.RESET} (for -help) Get info about command (example, python -m disspy -help -version)
+                {colorama.Fore.YELLOW}-no-color, -NC ->{colorama.Fore.RESET} Print info without colorama
+                """
+
+_helpcommand_no_color = """
+            Commands:
+                -help, -H -> Command with information about other commands
+                -version, -V -> Version of package
+            Params for commands (example, python -m disspy -help -no-color):
+                -[command] -> (for -help) Get info about command (example, python -m disspy -help -version)
+                -no-color, -NC -> Print info without colorama
+                """
 
 try:
     _op = argv[1]
 
     if _op in ['-help', '-H']:
-        print(f"""
-            {colorama.Fore.YELLOW}-help, -H ->{colorama.Fore.RESET} Command with information about other commands
-            -version, -V -> Version of package
-            """)
+        try:
+            _param = argv[2]
+
+            if _param in ['-version', '-V', '-help', '-H']:
+                try:
+                    _secparam = argv[3]
+
+                    if _secparam in ['-no-color', '-NC']:
+                        # -[command]
+                        if _param in ['-version', '-V']:
+                            print("""
+                        -version, -V
+                            Show disspy version
+                                """)
+                        elif _param in ['-help', '-H']:
+                            print("""
+                        -help, -H
+                            Information about commands and params
+                                """)
+                except IndexError:
+                    # -[command]
+                    if _param in ['-version', '-V']:
+                        print(f"""
+                    {colorama.Fore.GREEN}-version, -V
+                        {colorama.Fore.RESET}Show disspy version
+                            """)
+                    elif _param in ['-help', '-H']:
+                        print(f"""
+                    {colorama.Fore.GREEN}-help, -H
+                        {colorama.Fore.RESET}Information about commands and params
+                            """)
+            # -no-color
+            elif _param in ['-no-color', '-NC']:
+                print(_helpcommand_no_color)
+            else:
+                print(_helpcommand)
+        except IndexError:
+            print(_helpcommand)
 
     elif _op in ['-version', '-V']:
-        print(f"{colorama.Fore.YELLOW}Dispy version -> {colorama.Fore.RESET}{__version__}")
-    
+        try:
+            _param = argv[2]
+            if _param in ['-no-color', '-NC']:
+                print(f"""
+                Dispy version -> {__version__}
+                CLI version   -> {__cli_version__}
+                """)
+
+        except IndexError:
+            print(f"""
+                {colorama.Fore.YELLOW}Dispy version -> {colorama.Fore.RESET}{__version__}
+                {colorama.Fore.YELLOW}CLI version   -> {colorama.Fore.RESET}{__cli_version__}
+                """)
+
     else:
         print(f"""
-              {colorama.Fore.RED}                Error!{colorama.Fore.YELLOW} Invalid param!
-        {colorama.Fore.RESET}If you don't know params type `python -m disspy -H` or `python -m disspy -help`
+                              {colorama.Back.RED}Error!{colorama.Back.RESET}{colorama.Fore.YELLOW} Invalid command!
+        {colorama.Fore.RESET}If you don't know commands type `python -m disspy -H` or `python -m disspy -help`
                                                                                 Error code: 1101
               """)
+
 except IndexError:
     print(f"""
-        {colorama.Fore.RED}                Error!{colorama.Fore.YELLOW} Please type any param!
-        {colorama.Fore.RESET}If you don't know params type `python -m disspy -H` or `python -m disspy -help` 
+                        {colorama.Back.RED}Error!{colorama.Back.RESET}{colorama.Fore.YELLOW} Please type any command!
+        {colorama.Fore.RESET}If you don't know commands type `python -m disspy -H` or `python -m disspy -help` 
                                                                                 Error code: 1100
           """)
