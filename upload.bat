07c4f9acdf3866dc383e45a8f147c229d792ba2d
itttgg <spdd5724@gmail.com>
2022-08-02 21:43:13 +0300

    upload.bat update

--- upload.bat
+++ upload.bat
@@ -2,11 +2,20 @@
 pip install --upgrade --user pip
 pip install setuptools twine
 python setup.py sdist
+
 twine upload dist\*
+
 del disspy.egg-info\*
-del dist\*
-del build\*
 rmdir disspy.egg-info
+
+del dist\*
 rmdir dist
+
+del build\lib\disspy\*
+rmdir build\lib\disspy
+del build\lib\*
+rmdir build\lib
+del build\*
 rmdir build
+
 pause
