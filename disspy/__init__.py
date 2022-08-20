839ac4be315e876c976ff5338e00ec5aa9ba5b65
itttgg <spdd5724@gmail.com>
2022-08-20 20:25:10 +0300

    Bump version to 0.6.2.1

--- disspy/__init__.py
+++ disspy/__init__.py
@@ -124,7 +124,7 @@ def _all_generator(alls: list) -> tuple:
 # Variables
 
 # Version of disspy
-__version__ = "0.6.1 dev"
+__version__ = "0.6.2.1"
 
 # Link to GitHub repo
 __github__ = "https://github.com/itttgg/dispy"
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.md", "r", encoding="utf-8") as mdf:
 requires = ['aiohttp>=3.6.0,<4', 'requests', 'asyncio', 'colorama']
 
 
-__version__ = "0.6.1"
+__version__ = "0.6.2.1"
 
 
 setup(
