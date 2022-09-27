71d87d69926ce89e6536f22c5dfb492bd4129025
itttgg <spdd5724@gmail.com>
2022-09-27 23:50:42 +0300

    Install new theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,10 @@
+import sphinx_bootstrap_theme
+
 extensions = []
 html_logo = "l.png"
 html_favicon = 'favicon.ico'
-
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 html_theme_options = {
     'logo_only': False,
