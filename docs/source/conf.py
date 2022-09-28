14d3ebb5a0b3aa39e89aa3f6b24b088f22a802b1
itttgg <spdd5724@gmail.com>
2022-09-28 12:36:03 +0300

    Customize header

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,7 @@ extensions = []
 html_logo = "l.png"
 html_favicon = 'favicon.ico'
 html_theme = 'sphinx_book_theme'
+templates_path = ["_templates"]
 
 html_theme_options = {
     "logo_only": True,
@@ -10,5 +11,6 @@ html_theme_options = {
     "use_repository_button": True,
     "use_download_button": False,
     "use_fullscreen_button": True,
-    "use_issues_button": True
+    "use_issues_button": True,
+    "use_edit_page_button": True
 }
