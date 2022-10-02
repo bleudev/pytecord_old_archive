0ced67198128dd9de43d87745ad7915b6406841b
itttgg <spdd5724@gmail.com>
2022-10-02 12:45:18 +0300

    Update examples №2

--- examples/events/on_message.py
+++ examples/events/on_message.py
@@ -4,20 +4,38 @@ bot = disspy.DisBot(token="TOKEN", flags=disspy.DisFlags.messages())  # Create b
 
 
 # "On message create" event
-@bot.on(disspy.DisBotEventType.ON_MESSAGEC)
+@bot.on("messagec")
+async def on_messagec(message: disspy.DisMessage):
+    if message.content == "!help":
+        await message.channel.send("Help command*")
+
+# on_message()
+@bot.on_message("create")
 async def on_messagec(message: disspy.DisMessage):
     if message.content == "!help":
         await message.channel.send("Help command*")
 
 
+
 # "On message update" event
-@bot.on(disspy.DisBotEventType.ON_MESSAGEU)
+@bot.on("messageu")
+async def on_messageu(message: disspy.DisMessage):
+    await message.reply("You updated this message!")
+
+# on_message()
+@bot.on_message("update")
 async def on_messageu(message: disspy.DisMessage):
     await message.reply("You updated this message!")
 
 
+
 # "On message delete" event
-@bot.on(disspy.DisBotEventType.ON_MESSAGED)
+@bot.on("messaged")
+async def on_messaged(e: disspy.MessageDeleteEvent):
+    await e.channel.send("Message deleted!")
+
+# on_message()
+@bot.on_message("delete")
 async def on_messaged(e: disspy.MessageDeleteEvent):
     await e.channel.send("Message deleted!")
 
