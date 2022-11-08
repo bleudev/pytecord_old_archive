3d4d0dfb78f2bc75d5494b3be41def784adc2e2c
PixelDe <95937737+itttgg@users.noreply.github.com>
2022-11-08 11:34:49 +0300

    Commit suggestion №19

--- examples/events/on_ready.py
+++ examples/events/on_ready.py
@@ -5,7 +5,7 @@ client = disspy.Client(token="TOKEN")  # Create bot
 
 # on_ready()
 @client.on_ready()
-async def on_ready():
+async def ready():
     print("Logged as", client.user.fullname)  # For example "Logged as Dispy#0000"
 
 # event()
