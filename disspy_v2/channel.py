6816f38522680fed027d3a417aeaefcee37dfced
itttgg <spdd5724@gmail.com>
2022-12-21 17:03:40 +0300

    Add all parameters in messages

--- disspy_v2/channel.py
+++ disspy_v2/channel.py
@@ -18,8 +18,34 @@ class Message:
         print('DATA:', data['channel_id'])
         self._session = session
         self._sender = _MessageSender(session)
-        self.id = data.get('id', None)
+        
+        # Json paramenters
+        self.id: int = data.get('id')
+        self.channel_id: int = data.get('channel_id')
+        self.author = data.get('author', None) # todo: Add support for users
         self.content = data.get('content', None)
+        self.timestamp: str = data.get('timestamp')
+        self.edited_timestamp: str | None = data.get('edited_timestamp', None)
+        self.tts: bool = data.get('tts')
+        self.mention_everyone: bool = data.get('mention_everyone')
+        self.mentions: list[dict] = data.get('mentions', []) # todo: Add support for users
+        self.mention_roles: list[dict] = data.get('mention_roles', []) # todo: Add support for roles
+        self.mention_channels: list[dict] = data.get('mention_channels', []) # todo: Add assign mention channels
+        self.attachments: list[dict] = data.get('attachments', []) # todo: Add support for attachments
+        self.embeds: list[dict] = data.get('embeds', []) # todo: Add support for embeds 
+        self.reactions: dict | None = data.get('reactions', None) # todo: Add support for reactions
+        self.pinned: bool = data.get('pinned')
+        self.webhook_id: int | None = data.get('webhook_id', None)
+        self.type: int = data.get('type')
+        self.application_id: int | None = data.get('application_id', None)
+        self.message_reference: data | None = data.get('message_reference', None) # todo: Add support for message reference
+        self.flags: int | None = data.get('flags', None)
+        self.referenced_message: dict | None = data.get('referenced_message', None) # todo: Add assign referenced message
+        self.interaction: dict | None = data.get('interaction', None) # todo: Add support for interactions
+        self.thread: dict | None = data.get('thread', None) # todo: Add assign thread
+        self.components: list[dict] = data.get('components', [])
+        self.stickers: list[dict] = data.get('stickers', []) # todo: Add support for stickers
+        self.position: int | None = data.get('position', None)
 
         channel_id = data['channel_id']
         channel_json = self._sender.get(URL+'/channels/'+channel_id, session.headers)
