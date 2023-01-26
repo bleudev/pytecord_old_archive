95ac9328767728921bc60fdbb8611f8d78ea9482
itttgg <spdd5724@gmail.com>
2023-01-26 19:27:00 +0300

    Ready example

---
+++ examples_v2/events/ready.py
@@ -0,0 +1,18 @@
+"""
+`ready.py`
+
+In this file you will learn to use `ready` event
+
+Enjoy!
+"""
+
+# Create a client
+from disspy_v2 import Client
+client = Client(token='token') # Replace with your token
+
+# For using events you can use `event` decorator
+@client.event()
+async def ready():
+    print("Hello, i'm ready :3") # Print message on console
+
+client.run()
