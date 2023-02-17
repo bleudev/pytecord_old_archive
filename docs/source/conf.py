b9ddfb0756785236166c05a82440e9eb8b0ce3ef
itttgg <spdd5724@gmail.com>
2023-02-18 01:01:06 +0300

    bug fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,16 @@
 extensions = ["myst_parser"]
+
+myst_enable_extensions = [
+    'colon_fence',
+    'deflist',
+    'dollarmath'
+]
+myst_heading_anchors = 2
+
+project = 'disspy'
 html_logo = "l.png"
 html_favicon = 'favicon.ico'
 html_theme = 'furo'
-html_static_path = ["_static"]
-project = 'disspy'
 
 html_theme_options = {
     "source_repository": "https://github.com/pixeldeee/disspy",
@@ -11,3 +18,6 @@ html_theme_options = {
     "source_directory": "docs/",
     "repository_branch": "master"
 }
+
+html_static_path = ["_static"]
+templates_path = ["_templates"]
