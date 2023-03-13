c2dba260473bb983adc9c7d86b6a276335801a11
itttgg <spdd5724@gmail.com>
2023-03-13 10:48:18 +0300

    Renaming

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
-"""
-MIT License
-
-Copyright (c) 2022 itttgg
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from setuptools import setup
+from pytecord import __version__
 
 with open('README.md', 'r', encoding='utf-8') as mdf:
     long_description = mdf.read()
@@ -30,19 +7,17 @@ with open('README.md', 'r', encoding='utf-8') as mdf:
 with open('requirements.txt', 'r', encoding='utf-8') as rqf:
     install_requires = rqf.readlines()
 
-__version__ = "0.7"
-
 setup(
-    name='disspy',
+    name='pytecord',
     version=__version__,
     author='pixeldeee',
     author_email='aitiiigg1@gmail.com',
-    description='Disspy - library for creating bots in discord written in Python',
-    download_url=f'https://github.com/pixeldeee/disspy/releases/tag/{__version__}',
+    description='Pytecord - library for creating bots in discord written in Python',
+    download_url=f'https://github.com/pixeldeee/pytecord/releases/tag/{__version__}',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/pixeldeee/disspy',
-    packages=['disspy'],
+    url='https://github.com/pixeldeee/pytecord',
+    packages=['pytecord'],
     classifiers = [
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.11',
