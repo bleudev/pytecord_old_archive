fe7e957a258289bcba36c7a391325c8274da2ce8
itttgg <spdd5724@gmail.com>
2023-04-02 16:28:07 +0300

    №2

--- pytecord/guild.py
+++ pytecord/guild.py
@@ -3,7 +3,6 @@ from typing import TYPE_CHECKING
 from pytecord.role import Role
 
 if TYPE_CHECKING:
-    from pytecord.annotations import Snowflake
     from pytecord.payloads import GuildPayload
 
 __all__ = (
@@ -13,26 +12,26 @@ __all__ = (
 class WelcomeScreen: ...
 
 class Guild:
-    def __init__(self, data: 'GuildPayload') -> None:
+    def __init__(self, session, data: 'GuildPayload') -> None:
         _ = data.get
 
-        self.id: 'Snowflake' = _('id')
+        self.id: int = int( _('id') )
         self.name: str = _('name')
         self.icon: str | None = _('icon')
         self.icon_hash: str | None = _('icon_hash')
         self.splash: str | None = _('splash')
         self.discovery_splash: str | None = _('discovery_splash')
-        self.owner: bool | None
-        self.owner_id: Snowflake
-        self.permissions: str | None
-        self.afk_channel_id: Snowflake | None
-        self.afk_timeout: int
-        self.widget_enabled: bool | None
-        self.widget_channel_id: Snowflake | None
-        self.verification_level: int
-        self.default_message_notifications: int
-        self.explicit_content_filter: int
-        self.roles: list[Role]
+        self.owner: bool | None = _('owner')
+        self.owner_id: int = int( _('owner_id') )
+        self.permissions: str | None = _('permissions')
+        self.afk_channel_id: int | None = int( _('afk_channel_id') )
+        self.afk_timeout: int = _('afk_timeout')
+        self.widget_enabled: bool | None = _('widget_enabled')
+        self.widget_channel_id: int | None = int( _('widget_channel_id') )
+        self.verification_level: int = _('verification_level')
+        self.default_message_notifications: int = _('default_message_notifications')
+        self.explicit_content_filter: int = _('explicit_content_filter')
+        self.roles: list[Role] = [Role(session, **i) for i in _('roles')]
         # emojis: list[Emoji] TODO: Add emoji support
         self.features: list[str]
         self.mfa_level: int
